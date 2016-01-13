a2c9205f2267e3a59602eae680992340697172d0
Maximiliano Pombo <maxipombo@gmail.com>
2016-01-13 19:40:48 -0300

    Find by title, in stock and include puts

--- lib/product.rb
+++ lib/product.rb
@@ -1,5 +1,5 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :stock, :price
 
   public
 
@@ -7,11 +7,25 @@ class Product
 
   def initialize(options={})
     @title = options[:title]
+    @stock = options[:stock]
+    @price = options[:price]
     add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  def self.find_by_title(title)
+    @@products.find {|product| product.title == title}
+  end
+
+  def self.in_stock
+    @@products.find {|product| product.in_stock}
+  end
+
+  def in_stock?
+    @stock > 0
   end
 
   private
