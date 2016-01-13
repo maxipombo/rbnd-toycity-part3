5defcdbe18bb9c653b504ec6e9eeae5bbcd2cb91
Maximiliano Pombo <maxipombo@gmail.com>
2016-01-13 18:07:39 -0300

    Create add to products if not already exists

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,26 @@
 class Product
+  attr_reader :title
+
+  public
+
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    add_to_products
   end
 
   def self.all
     @@products
+  end
+
+  private
+
+  def add_to_products
+    if @@products.to_s.include? @title
+      raise DuplicateProductError, "'#{title}' already exists."
+    else
+      @@products << self
+    end
   end
 end
