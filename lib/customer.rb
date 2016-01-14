0c7095c1abf50a82ada2ed2c9f9d3ed74ecdffad
Maximiliano Pombo <maxipombo@gmail.com>
2016-01-14 17:36:08 -0300

    Add customers

---
+++ lib/customer.rb
@@ -0,0 +1,32 @@
+class Customer
+  attr_reader :name
+
+  public
+
+  @@customers = []
+
+  def initialize(options={})
+    @name = options[:name]
+    add_to_customers
+  end
+
+  def self.all
+    @@customers
+  end
+
+# Search a customer by name
+  def self.find_by_name(name)
+    @@customers.find {|product| product.name == name}
+  end
+
+  private
+
+# Add a customer only if doesn't exist
+  def add_to_customers
+    if @@customers.to_s.include? @name
+      raise DuplicateCustomerError, "'#{name}' already exists."
+    else
+      @@customers << self
+    end
+  end
+end
